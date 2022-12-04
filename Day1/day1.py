1b24577a22058d1e1957f7f957e6c221b56f8e47
John Doe <johndoe@example.com>
2022-12-04 13:32:36 -0700

    Passes Part 2

--- Day1/day1.py
+++ Day1/day1.py
@@ -2,7 +2,6 @@
 calories = 0
 items = 0
 max_calories = 0
-list_items = []
 list_calories = []
 with open("Day1/input.txt") as fp:
     for line in fp:
@@ -10,12 +9,10 @@ with open("Day1/input.txt") as fp:
             if calories > max_calories:
                 max_calories = calories
             list_calories.append(calories)
-            list_items.append(items)
             calories = 0
             items = 0
         else:
-            items += 1
             calories += int(line)
     list_calories.append(calories)
-    list_items.append(items)
-    print(max(list_calories))
+    list_calories.sort()
+    print(sum(list_calories[-3: ]))
